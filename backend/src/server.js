e1dda199ab595a4fed7ee1358deddf84ab8564ba
Sanchay Jadon <jadonsanchay@gmail.com>
2025-08-07 10:37:17 +0530

    feat: Implement authentication flow with routing and onboarding

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,8 @@ import cors from "cors";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-// import userRoutes from "./routes/user.route.js";
-// import chatRoutes from "./routes/chat.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -26,8 +26,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/chat", chatRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
---
+++ frontend/src/pages/NotificationsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotificationsPage = () => {
+  return (
+    <div>
+      Notication
+    </div>
+  )
+}
+
+export default NotificationsPage
